616374362b80b234c75866659b8deddd4daa7361
reshmapradh <reshma.pr28@gmail.com>
2026-07-29 15:00:38 -0400

    changing base url to render

--- src/components/articleService.js
+++ src/components/articleService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/articles";
+const BASE_URL = "https://newswebsite-backend-1.onrender.com";
 
 export const getArticles = () => {
   return axios.get(BASE_URL);

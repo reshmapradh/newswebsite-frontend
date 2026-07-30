340b2ff4d08600df1f1092cc61d478f7a30599a1
reshmapradh <reshma.pr28@gmail.com>
2026-07-30 17:34:26 -0400

    committing changes in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,29 @@
 import { useEffect, useState } from "react";
-import { getArticles } from "./articleService";
-
-import { useEffect, useState } from "react";
 import { getArticles } from "../api/articleService";
 
 import Navbar from "../components/Navbar";
 import PostCard from "../components/PostCard";
 
-export default function Home(){
+export default function Home() {
+  const [articles, setArticles] = useState([]);
 
-    const [articles,setArticles] = useState([]);
+  useEffect(() => {
+    getArticles()
+      .then((response) => {
+        setArticles(response.data);
+      })
+      .catch(console.error);
+  }, []);
 
-    useEffect(()=>{
+  return (
+    <>
+      <Navbar />
 
-        getArticles()
-            .then(response=>{
-                setArticles(response.data);
-            })
-            .catch(console.error);
-
-    },[]);
-
-    return(
-
-        <>
-            <Navbar/>
-
-            <div className="grid grid-cols-3 gap-8">
-
-                {articles.map(article=>(
-                    <PostCard
-                        key={article.id}
-                        post={article}
-                    />
-                ))}
-
-            </div>
-
-        </>
-
-    )
-
+      <div className="grid grid-cols-3 gap-8">
+        {articles.map((article) => (
+          <PostCard key={article.id} post={article} />
+        ))}
+      </div>
+    </>
+  );
 }
-
-export default Home;
